ae2094b574570d1838d4afd79ae8c668170d32c9
Miguel Patino <mpatino06@hotmail.com>
2021-02-28 12:56:51 -0500

    AuthRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,22 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 
 //Modulos
 import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: '**', component: NopagefoundComponent }
 
+const routes: Routes = [
+  //path: /dashboard PagesRoutingModule
+  //path: /auth AuthRoutingModule
+  { path: '**', component: NopagefoundComponent }
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    PagesRoutingModule
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [RouterModule]
 
